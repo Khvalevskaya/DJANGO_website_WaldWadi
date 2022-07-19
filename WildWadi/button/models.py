ec3279c25cebe00afc224c1e87c03616618e8d43
Yuliya <khvalevskaya@icloud.com>
2022-07-19 23:06:42 +0300

    changed form

--- WildWadi/button/models.py
+++ WildWadi/button/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField("Name", max_length=100)
     full_text = models.TextField("Description")
-    price = models.CharField("Price", max_length=100)
+    price = models.CharField("Price", max_length=200)
     date = models.DateTimeField("Date")
 
     def __str__(self):
--- WildWadi/button/views.py
+++ WildWadi/button/views.py
@@ -4,9 +4,9 @@ from .models import Articles
 
 def button_home(request):
     button = Articles.objects.all()
-    tickets = {"standart_adult": [249, 100],
-               "standart_child": [219, 100],
-               "combo_adult": [275, 100],
-               "combo_child": [255, 100]}
 
-    return render(request, "button/button_home.html", {"button": button},)
+    # tickets = {"standart_adult": [249, 100],
+    #            "standart_child": [219, 100],
+    #            "combo_adult": [275, 100]}
+
+    return render(request, "button/button_home.html", {"button": button})
